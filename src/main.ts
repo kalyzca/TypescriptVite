3169064af0a1ce718270e78cbde792f414518430
Kaly <alcantarakaly@gmail.com>
2025-05-06 00:40:44 -0500

    [ADD] tarea de interfaces

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import './style.css';
+
 // import './Topics/01-basic-types'; //* La extensión ts esta sobreentendida 
 // import './Topics/02-object-interface';
 // import './Topics/03-functions';
-import  './Topics/04-functions-with-arguments';
+// import  './Topics/04-functions-with-arguments';
+import './Topics/05-homework-types';
 const app =document.querySelector<HTMLDivElement>('#app');
 app!.innerHTML="Hola mundo!";
 console.log(app);
